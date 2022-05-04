5c151d6cb422867b8246b5a1ec4d2f0785954597
SydVic <fabrizio.bocca27@gmail.com>
2022-05-04 14:21:40 +0200

    implementata logica base ma supera 50

--- JSnack2/js/script.js
+++ JSnack2/js/script.js
@@ -1,4 +1,15 @@
 // creare array vuoto
-
+const originalArray = [];
+let originalArrayNumbersSum = 0;
 // chiedere all'utente i numeri da inserire nell'array
+do {
+    const userNumber = parseInt(prompt("Dimmi un numero.."));
+    originalArray.push(userNumber);
+
+    originalArrayNumbersSum += userNumber;
+
 // farlo fino a quando la somma degli elementi è inferiore a 50
+} while ( originalArrayNumbersSum < 50 );
+
+console.log("originalArray" ,originalArray);
+console.log("originalArrayNumbersSum" ,originalArrayNumbersSum);
