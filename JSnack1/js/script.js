713f624e7d6ab8c36cbce6622abc058e84c7d3e2
SydVic <fabrizio.bocca27@gmail.com>
2022-05-04 14:05:01 +0200

    aggiunto ciclo do while per far combaciare lunghezza array

--- JSnack1/js/script.js
+++ JSnack1/js/script.js
@@ -4,13 +4,19 @@ const secondArray = [67, 29, 20, 3, 78, 21, 84, 18];
 
 // pushare elementi nell'array più corto fino a che entrambi hanno la stessa lunghezza
     // capire quale array è più corto
-    let shortestArray = [];
-    if (firstArray.length < secondArray.length) {
-        shortestArray = firstArray;
-    } else {
-        shortestArray = secondArray;
-    }
+let shortestArray = [];
+if (firstArray.length < secondArray.length) {
+    shortestArray = firstArray;
+} else {
+    shortestArray = secondArray;
+}
 
-    console.log("shortestArray", shortestArray);
+console.log("shortestArray", shortestArray);
     // pushare elementi nell'array più corto
     // fino a che il "controllo" ci dice che hanno la stessa lunghezza
+do {
+    const newArrayElement = Math.floor(Math.random() * 100) + 1;
+    shortestArray.push(newArrayElement);
+} while (shortestArray.length != secondArray.length);
+
+console.log("shortestArray", shortestArray);
