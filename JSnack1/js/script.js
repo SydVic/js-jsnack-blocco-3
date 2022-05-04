c1018a14478110cb65806045a1d18466be369cd7
SydVic <fabrizio.bocca27@gmail.com>
2022-05-04 13:57:22 +0200

    pseudocodice e metodo per stabilire array più corto

--- JSnack1/js/script.js
+++ JSnack1/js/script.js
@@ -1,1 +1,16 @@
-alert("hello");
+// creare due array di lunghezze diverse
+const firstArray = [3, 45, 72, 19, 95];
+const secondArray = [67, 29, 20, 3, 78, 21, 84, 18];
+
+// pushare elementi nell'array più corto fino a che entrambi hanno la stessa lunghezza
+    // capire quale array è più corto
+    let shortestArray = [];
+    if (firstArray.length < secondArray.length) {
+        shortestArray = firstArray;
+    } else {
+        shortestArray = secondArray;
+    }
+
+    console.log("shortestArray", shortestArray);
+    // pushare elementi nell'array più corto
+    // fino a che il "controllo" ci dice che hanno la stessa lunghezza
